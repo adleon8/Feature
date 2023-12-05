599d8e52b245928b8320c5068dc4066d6259ab6f
adleon8 <daniella.leonochoa@gmail.com>
2023-12-04 17:17:56 -0800

    Updates

--- HorseFeature/Assets/Scripts/Player.cs
+++ HorseFeature/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Player : MonoBehaviour
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float mass = 1f;
+    [SerializeField] float acceleration = 20f;
     [SerializeField] Transform cameraTransform;
 
     // To have the ability to look around.
@@ -17,21 +19,51 @@ public class Player : MonoBehaviour
     // Player's velocity.
     Vector3 velocity;
 
+    // Input system.
+    PlayerInput playerInput;
+    InputAction moveAction;
+    InputAction lookAction;
+    InputAction jumpAction;
+
     private void Awake()
     {
         // References the character component in the game object.
         controller = GetComponent<CharacterController>();
+
+        // References the player input actions.
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["move"];
+        lookAction = playerInput.actions["look"];
+        jumpAction = playerInput.actions["jump"];
+
     }
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Movement();
         Look();
         Gravity();
+    }
+    
+    // STAY HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    private void SlopeSliding()
+    {
+        //if (Is)
+    }
+
+    private void GroundUpdate()
+    {
+        SlopeSliding();
+
+        if (wasGrounded != IsGrounded)
+        {
+            OnGroundStateChange?.Invoke(IsGrounded);
+            wasGrounded = IsGrounded;
+        }
     }
 
     private void Gravity()
@@ -43,40 +75,56 @@ public class Player : MonoBehaviour
         velocity.y = controller.isGrounded ? -1f : velocity.y + gravity.y;
     }
 
-    private void Movement()
+    Vector3 GetMovementInput()
     {
-        // Takes the input of the horizontal and vertical axis.
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
+        // Takes the input and reads the value methods of moving actions.
+        var moveInput = moveAction.ReadValue<Vector2>();
 
         // Input Vector.
         var input = new Vector3();
 
         // Adds vector pointing forward. 
-        input += transform.forward * y;
+        input += transform.forward * moveInput.y;
 
         // Adds vector pointing to the right.
-        input += transform.right * x;
+        input += transform.right * moveInput.x;
 
         // Clamping it to 1 so the vector doesn't move faster diagonally than normally. 
         input = Vector3.ClampMagnitude(input, 1f);
 
-        // Moves the player by calling transform translate. Makes the movement not frame rate dependant.
-        //transform.Translate(input * movementSpeed * Time.deltaTime, Space.World);
+        // Takes in player's input and activates a smoother movement.
+        input *= movementSpeed;
+
+        return input;
+    }
+
+    private void Movement()
+    {
+        var input = GetMovementInput();
+
+        // Uses acceleration to prevent player's movement from looking too abrupt.
+        // Gradually interpolates back to zero or up to the input being pressed.
+        var factor = acceleration * Time.deltaTime;
+        velocity.x = Mathf.Lerp(velocity.x, input.x, factor);
+        velocity.z = Mathf.Lerp(velocity.z, input.z, factor);
 
         // Allows jumping.
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        var jumpInput = jumpAction.ReadValue<float>();
+        if (jumpInput > 0 && controller.isGrounded)
         {
             // Add the jumping height to the y axis velocity to lift player off the ground.
             velocity.y += jumpSpeed;
         }
 
         // Moves the player but uses the controller instead.
-        controller.Move((input * movementSpeed + velocity) * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
     }
 
     private void Look()
     {
+        // Takes player input for looking.
+        var lookInput = lookAction.ReadValue<Vector2>();
+
         // Checks where the mouse position is going on the X and Y axis.
         look.x += Input.GetAxis("Mouse X");
         look.y += Input.GetAxis("Mouse Y");
